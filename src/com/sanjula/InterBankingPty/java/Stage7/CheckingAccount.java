a4f5a9a32f91f62f39de419d76151bfc7d5c45a7
Sanjula Madurapperuma <sanjula99@gmail.com>
2018-03-31 09:04:38 +0530

    9.04am 31/3/2018
    Made some corrections according to requirements :
    BankAccount class
    Added functionality to Savings Account and Checking Account
    Added Functionality to CheckingAccountWithInterest Class

--- src/com/sanjula/InterBankingPty/java/Stage7/CheckingAccount.java
+++ src/com/sanjula/InterBankingPty/java/Stage7/CheckingAccount.java
@@ -26,6 +26,54 @@ public class CheckingAccount extends BankAccount{
 
     }
 
+    @Override
+    protected CheckingAccount enterAccountData(Customer customer){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println();
+        System.out.println("=================================");
+        System.out.println("Please enter details of the Bank Branch");
+        System.out.println();
+        System.out.print("Enter BSB Number : ");
+        while(!sc.hasNextInt()){
+            System.out.println("Please enter a 6-digit number");
+            sc.next();
+        }
+        int bsbNumber = sc.nextInt();
+        System.out.println("Enter the address : ");
+        String address = sc.nextLine();
+        System.out.println("Enter the postcode : ");
+        while(!sc.hasNextInt()){
+            System.out.println("Please enter a valid postcode number");
+            sc.next();
+        }
+        int postcode = sc.nextInt();
+
+        //Filling the homeBranch object
+        homeBranch.setBsbNumber(bsbNumber);
+        homeBranch.setAddress(address);
+        homeBranch.setPostcode(postcode);
+
+
+        double accBalance = Main.getAccountBalance(sc, "Enter the opening account balance ($): ");
+        while(accBalance < 0 || accBalance > 100000){
+            accBalance = Main.getAccountBalance(sc, "Enter valid opening account balance ($) : ");
+        }
+
+        CheckingAccount account = new CheckingAccount(accountNumber, accBalance,
+                homeBranch, monthlyFee, noOfChecksAllowed);
+        Main.bankAccountList.add(account);
+        if (customer != null){
+            customer.setBankAccountsList(Main.bankAccountList);
+        }
+        Main.customerList.add(customer);
+        Main.dataPersistency(Main.customerList);
+        System.out.println("=====Bank Account created successfully.=====");
+        System.out.println();
+
+        return account;
+    }
+
     public void display(CheckingAccount account){
         System.out.println("=====The details of your new Checking account are : =====");
         System.out.println();
--- src/com/sanjula/InterBankingPty/java/Stage7/SavingsAccount.java
+++ src/com/sanjula/InterBankingPty/java/Stage7/SavingsAccount.java
@@ -1,5 +1,7 @@
 package com.sanjula.InterBankingPty.java.Stage7;
 
+import java.util.Scanner;
+
 public class SavingsAccount extends BankAccount {
     protected double interestRate;
 
@@ -21,6 +23,54 @@ public class SavingsAccount extends BankAccount {
 
     public void setInterestRate(double interestRate) {
         this.interestRate = interestRate;
+    }
+
+    @Override
+    protected SavingsAccount enterAccountData(Customer customer){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println();
+        System.out.println("=================================");
+        System.out.println("Please enter details of the Bank Branch");
+        System.out.println();
+        System.out.print("Enter BSB Number : ");
+        while(!sc.hasNextInt()){
+            System.out.println("Please enter a 6-digit number");
+            sc.next();
+        }
+        int bsbNumber = sc.nextInt();
+        System.out.println("Enter the address : ");
+        String address = sc.nextLine();
+        System.out.println("Enter the postcode : ");
+        while(!sc.hasNextInt()){
+            System.out.println("Please enter a valid postcode number");
+            sc.next();
+        }
+        int postcode = sc.nextInt();
+
+        //Filling the homeBranch object
+        homeBranch.setBsbNumber(bsbNumber);
+        homeBranch.setAddress(address);
+        homeBranch.setPostcode(postcode);
+
+
+        double accBalance = Main.getAccountBalance(sc, "Enter the opening account balance ($): ");
+        while(accBalance < 0 || accBalance > 100000){
+            accBalance = Main.getAccountBalance(sc, "Enter valid opening account balance ($) : ");
+        }
+
+        SavingsAccount account = new SavingsAccount(accountNumber, accBalance,
+                interestRate, homeBranch);
+        Main.bankAccountList.add(account);
+        if (customer != null){
+            customer.setBankAccountsList(Main.bankAccountList);
+        }
+        Main.customerList.add(customer);
+        Main.dataPersistency(Main.customerList);
+        System.out.println("=====Bank Account created successfully.=====");
+        System.out.println();
+
+        return account;
     }
 
     protected void displayAccount(SavingsAccount account){
